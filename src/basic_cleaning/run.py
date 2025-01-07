6b77e709a8687e16604f207d7070c9be5c1992b9
yurimarca <yurimarca@gmail.com>
2025-01-07 13:15:19 -0300

    Cleaning step done, but there are some NaN values in the clean_data

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -5,6 +5,9 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 import argparse
 import logging
 import wandb
+import tempfile
+
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -13,16 +16,50 @@ logger = logging.getLogger()
 
 def go(args):
 
-    run = wandb.init(job_type="basic_cleaning")
-    run.config.update(args)
+    with tempfile.NamedTemporaryFile(mode='wb+') as fp:
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+        logger.info("Basic cleaning step")
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+        run = wandb.init(job_type="basic_cleaning")
+        run.config.update(args)
+
+        # Download input artifact. This will also log that this script is using this
+        # particular version of the artifact
+        # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+        logger.info("Downloading artifact: %s", args.input_artifact)
+        artifact = run.use_artifact(args.input_artifact)
+        artifact_path = artifact.file()
+
+        df = pd.read_csv(artifact_path)
+
+        logger.info("Cleaning artifact: %s", args.input_artifact)    
+        # Price range
+        idx = df['price'].between(args.min_price, args.max_price)
+        df = df[idx].copy()
+        # Duplicates
+        df = df.drop_duplicates().reset_index(drop=True)
+
+        # Save dataframe to temp file
+        df.to_csv(fp.name, header=True, index=False)
+
+        # Log artifact to W&B
+        artifact = wandb.Artifact(
+            name=args.output_artifact,
+            type=args.output_type,
+            description=args.output_description,
+        )
+        artifact.add_file(fp.name)
+
+        logger.info("Logging artifact: %s", args.output_artifact)
+        run.log_artifact(artifact)
+
+        if df.isna().sum().sum() > 0:
+            nans = df.isna().sum().sum()
+            logger.warning(f"WARNING: There are {nans} NaN cells in the cleaned data.")
+
+        run.finish()
+
 
 
 if __name__ == "__main__":
